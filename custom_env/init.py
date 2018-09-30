71c11b1c03b7b355ad28a6e51cf71f7ef110d18d
NateScarlet <NateScarlet@Gmail.com>
2018-09-30 11:03:17 +0800

    Update

---
+++ custom_env/init.py
@@ -0,0 +1,39 @@
+# -*- coding=UTF-8 -*-
+"""Disable nuke crash handling.  """
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+def _main():
+    import logging
+    import os
+    import subprocess
+    import sys
+    try:
+        import configparser
+    except ImportError:
+        import ConfigParser as configparser
+
+    __dirname__ = os.path.abspath(os.path.dirname(__file__))
+
+    config = configparser.SafeConfigParser()
+    config.optionxform = unicode
+    config.read([os.path.join(__dirname__, 'config.ini')])
+
+    def _set_env(key, value):
+        if sys.platform == 'win32':
+            subprocess.call(['setx', key, value])
+        os.environ[key] = value
+        logging.debug('Set enviroment variable: %s = %s', key, value)
+
+    for k, v in config.items('env'):
+        _set_env(k, v)
+
+    logging.info('Disabled nuke crash handling.')
+
+
+if __name__ == '__main__':
+    _main()
+
+del _main
--- init.py
+++ init.py
@@ -13,6 +13,7 @@ nuke.pluginAddPath('tabtabtab')
 nuke.pluginAddPath('ofx')
 nuke.pluginAddPath('batchrender', addToSysPath=False)
 nuke.pluginAddPath('olm_smoother')
+nuke.pluginAddPath('custom_env')
 nuke.pluginAddPath('//server/Scripts/NukePlugins/EE')
 if nuke.env['NukeVersionString'].startswith('10.5v'):
     nuke.pluginAddPath('optical_flares')
